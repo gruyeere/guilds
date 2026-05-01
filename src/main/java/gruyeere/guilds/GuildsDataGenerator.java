78a57c5dc67e75890c69d9d8b35a8957dc021958
gruyeere <gruyereski@gmail.com>
2026-05-01 08:35:17 +0800

    Initial commit

---
+++ src/main/java/gruyeere/guilds/GuildsDataGenerator.java
@@ -0,0 +1,11 @@
+package gruyeere.guilds;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GuildsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
